d38c1df4495e8295bd5211341ae1e1f848d8a2c3
KASIMEMRE <162051254+KASIMEMRE@users.noreply.github.com>
2025-02-27 22:30:22 +0300

    Döngüler(2)

---
+++ Döngüler(2)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime;
+//while
+
+Console.WriteLine("Lütfen bir sayı giriniz: ");
+int sayi = int.Parse(Console.ReadLine());
+int sayac = 1;
+int toplam = 0;
+while (sayac <= sayi)
+{
+    toplam += sayac;
+    sayac++;
+}
+Console.WriteLine("Toplam: " + toplam);
+
+char character = 'a';
+while (character <= 'z')
+{
+    Console.WriteLine(character);
+    character++;
+}
+
+Console.WriteLine("*******FOREACH********");
+
+string [] arabalar = {"BMW", "Mercedes", "Audi", "Toyota"};
+
+foreach (var araba in arabalar)
+{
+    Console.WriteLine(araba);
+}
