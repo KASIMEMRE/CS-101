a59bdc2acac95133de61400aba08f6c4c4558b17
KASIMEMRE <162051254+KASIMEMRE@users.noreply.github.com>
2025-02-27 22:17:38 +0300

    Döngüler(1)

---
+++ Döngüler(1)/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("Hello World!" + i);
+            }
+
+            Console.WriteLine("Kaç kere yazdırmak istersiniz?");
+            int sayac = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= sayac; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(  i);
+                }
+            }
+
+            int tekToplam = 0;
+            int ciftToplam = 0;
+            for (int i = 0; i <= 10; i++)
+            {
+                if (i % 2 == 1)
+                
+                     tekToplam += i;
+                
+                else
+                
+                    ciftToplam += i;
+                
+              Console.WriteLine("Tek Toplam: " + tekToplam);
+              Console.WriteLine("Çift Toplam: " + ciftToplam);
+            }
+
+            for (int i = 0; i <= 10; i++)
+            {
+                if (i == 4)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+            for (int i = 0; i <= 10; i++)
+            {
+                if (i == 4)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
