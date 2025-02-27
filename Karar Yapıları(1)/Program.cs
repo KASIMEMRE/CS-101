0f306eb2b528ec0246292ffe1fbef3565584cd19
KASIMEMRE <162051254+KASIMEMRE@users.noreply.github.com>
2025-02-27 21:58:49 +0300

    Karar Yapıları(1)

---
+++ Karar Yapıları(1)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int datetime = DateTime.Now.Hour;
+
+            if (datetime >= 6 && datetime < 12)
+            {
+                Console.WriteLine("Good Morning");
+            }
+            else if (datetime >= 12 && datetime < 18)
+            {
+                Console.WriteLine("Good Afternoon");
+            }
+            else if (datetime >= 18 && datetime < 24)
+            {
+                Console.WriteLine("Good Evening");
+            }
+            else
+            {
+                Console.WriteLine("Good Night");
+            }
+
+            string sonuc = datetime >= 6 && datetime < 12 ? "Good Morning" : datetime >= 12 && datetime < 18 ? "Good Afternoon" : datetime >= 18 && datetime < 24 ? "Good Evening" : "Good Night";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
