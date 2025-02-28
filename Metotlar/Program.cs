09222bc49a1a87be64949333949391d18d433845
KASIMEMRE <162051254+KASIMEMRE@users.noreply.github.com>
2025-02-28 20:30:06 +0300

    Metot Tanımlama

---
+++ Metotlar/Program.cs
@@ -0,0 +1,44 @@
+﻿//erişim_belirteci geri_dönüştipi metot_adı(parametreler)
+//{
+
+    //komutlar
+
+//}
+
+class Metotlar
+{
+    public void EkranaYazdir(string a){
+        Console.WriteLine(a);
+    }
+}
+
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int sonuc=Topla(12,34);
+            Console.WriteLine(sonuc);
+
+
+
+
+            Metotlar ornek = new Metotlar();
+        static void Main(string[] args)
+        {
+            int sonuc=Topla(12,34);
+            Console.WriteLine(sonuc);
+
+            Metotlar ornek = new Metotlar();
+            ornek.EkranaYazdir(Convert.ToString(sonuc));
+        }
+
+        static int Topla(int x,int y){
+            return x+y;
+        }
+        }
+        }
+        }
