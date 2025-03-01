45c3a062bb942fa26555f75b5e0118813e0f54ad
KASIMEMRE <162051254+KASIMEMRE@users.noreply.github.com>
2025-03-01 22:16:09 +0300

    soru2/soru3

---
+++ Soru 1/Soru 3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Soru - 3: Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde saklayan ve dizinin elemanlarını sıralayan programı yazınız.
+
+            Console.WriteLine("Bir cümle yazınız");
+            string cümle = Console.ReadLine();
+
+            foreach (var item in cümle)
+            {
+                
+                char[] sesliHarfler = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+                List<char> bulunanSesliHarfler = new List<char>();
+
+                if (sesliHarfler.Contains(item))
+                {
+                    bulunanSesliHarfler.Add(item);
+                }
+
+                bulunanSesliHarfler.Sort();
+
+                foreach (var harf in bulunanSesliHarfler)
+                {
+                    Console.Write(harf + " ");
+                }
+                    
+                
+            }
+        }
+    }
+}
