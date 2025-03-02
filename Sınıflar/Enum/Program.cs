4671b5993c8834bf2d6a843289ceec12c02d2706
KASIMEMRE <162051254+KASIMEMRE@users.noreply.github.com>
2025-03-02 14:47:30 +0300

    ...

---
+++ Sınıflar/Enum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //sıralı değerlerle kullanılabilir kodun okulabilirliğini artıran bir yapıdır
+
+
+            Console.WriteLine(Günler.cuma);
+            Console.WriteLine((int)Günler.cumartesi);
+
+            int sıcaklık=25;
+            if(sıcaklık<=(int)HavaDurumu.normal){
+                Console.WriteLine("Mangal Yapılabilir1");
+            }else if(sıcaklık>=(int)HavaDurumu.sıcak){
+                Console.WriteLine("Mangal Yapılabilir2");
+            }else{
+                Console.WriteLine("Mangal Yapılabilir3");
+            }
+                
+            
+        }
+    }
+    enum Günler{
+        pazartesi=1,
+        salı,
+        çarşamba,
+        perşembe,
+        cuma=29,
+        cumartesi,
+        pazar
+    }
+
+    enum HavaDurumu{
+        soğuk=5,
+        normal=15,
+        sıcak=30
+    }
+}
